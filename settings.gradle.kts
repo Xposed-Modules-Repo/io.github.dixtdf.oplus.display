07a0fabedb1ef7a83c7aec19b3e667f649645dee
root <root@root.com>
2025-06-01 14:52:42 +0800

    v1.0

---
+++ settings.gradle.kts
@@ -0,0 +1,26 @@
+pluginManagement {
+    repositories {
+        maven(url = "https://mirrors.huaweicloud.com/repository/maven/")
+        google {
+            content {
+                includeGroupByRegex("com\\.android.*")
+                includeGroupByRegex("com\\.google.*")
+                includeGroupByRegex("androidx.*")
+            }
+        }
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        maven(url = "https://mirrors.huaweicloud.com/repository/maven/")
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "allow-display-connect"
+include(":app")
+ 
